2ffbd09460126e26c431ed45f6a5cd005644f61f
Dr. Takeyuki Ueda <grenouill.vert@gmail.com>
2022-04-26 18:26:24 +0900

    Update response.go

--- response.go
+++ response.go
@@ -1,7 +1,14 @@
 package HttpSupplement
 
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+)
+
 func getJsonFromReadCloserWithLimitSize (body io.ReadCloser, limit int) (jsonObj map[string]interface{}, err error){
-  buff, err := ioutil.ReadAll(io.LimitReader(resp.Body, limit))
+  buff, err := ioutil.ReadAll(io.LimitReader(body, limit))
   if err != nil {
     log.Println(err)
 		return
@@ -15,4 +22,5 @@ func getJsonFromReadCloserWithLimitSize (body io.ReadCloser, limit int) (jsonObj
 
 func getJsonFromReadCloser (body io.ReadCloser) (jsonObj map[string]interface{}, err error){
   jsonObj, err = getJsonFromReadCloserWithLimitSize(body, 1024)
+	return
 }
