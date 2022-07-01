abbd82ff3fd2c6e63c153909539f67431aaf820a
Dr. Takeyuki Ueda <grenouill.vert@gmail.com>
2022-07-01 21:26:44 +0900

    Create json.go
    first version

---
+++ json.go
@@ -0,0 +1,17 @@
+package HttpSupplement
+
+/** 
+ Convert Float64 type data to Int64
+   
+   An Int64 data get converted to Float64 by Json marshaller.
+   So, convert back to Int64.
+*/
+func ConvertInt64IfFloat64(in interface{})(out interface{){
+  	switch v := in.(type) {
+		case float64:
+			out = int64(v)
+		default:
+			out = in
+		}
+    return
+}
